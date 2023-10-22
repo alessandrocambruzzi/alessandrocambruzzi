679e81f36e5cded0fa7f5e7683e69bf75d39b0bc
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-10-22 23:53:30 +0000

    fix: header edit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,11 @@
+import Image from 'next/image';
+
 export default function Header() {
-    return (
-        <header className="text-center text-white py-8 border-[#4d4d4d] md:flex itens-center justify-between">
-            <h4 id="home">🏫ESCOLA TECNOLÓGICA</h4>
-            <nav>
-                <a className="hover:text-[#00e77f]" href="#home">
-                    Home &nbsp;
-                </a>
-                <a className="hover:text-[#00e77f]" href="#cursos">
-                    Cursos &nbsp;
-                </a>
-                <a className="hover:text-[#00e77f]" href="#">
-                    Login &nbsp;{" "}
-                </a>
-                <a className="hover:text-[#00e77f]" href="#contato">
-                    Contato &nbsp;
-                </a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="text-center bg-white">
+      <p className="w-116 h-43 font-playfair font-extrabold text-4xl leading-43 mx-auto flex items-center text-[#0F172A]">
+        UTFPR<span className="text-red-500">.</span>
+      </p>
+    </header>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-
+import Banner from './components/Banner';
+import Categories from './components/Categories';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Trending from './components/Trending';
+import Latest from './components/Latest';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Banner />
-    </div>
-
-  )
-
+      <Categories />
+      <Trending />
+      <Latest />
+      <Footer />
+    </>
+  );
 }
